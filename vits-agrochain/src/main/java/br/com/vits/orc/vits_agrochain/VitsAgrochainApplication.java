0497bca896f037572e4e5b689714b01eada5c7de
joaoGFG <joaogabrielfg10@gmail.com>
2025-10-10 15:52:47 -0300

    Initialize VitsAgrochain project with Maven (Spring)

---
+++ vits-agrochain/src/main/java/br/com/vits/orc/vits_agrochain/VitsAgrochainApplication.java
@@ -0,0 +1,13 @@
+package br.com.vits.orc.vits_agrochain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitsAgrochainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitsAgrochainApplication.class, args);
+	}
+
+}
---
+++ vits-agrochain/src/test/java/br/com/vits/orc/vits_agrochain/VitsAgrochainApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vits.orc.vits_agrochain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitsAgrochainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

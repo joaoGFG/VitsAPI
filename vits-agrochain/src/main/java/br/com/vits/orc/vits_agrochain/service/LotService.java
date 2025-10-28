b715871536318e2ea28eda8704cdd0fe376ca9aa
joaoGFG <joaogabrielfg10@gmail.com>
2025-10-28 17:35:46 -0300

    feat: LotService

---
+++ vits-agrochain/src/main/java/br/com/vits/orc/vits_agrochain/service/LotService.java
@@ -0,0 +1,27 @@
+package br.com.vits.orc.vits_agrochain.service;
+
+import org.springframework.stereotype.Service;
+import br.com.vits.orc.vits_agrochain.model.Lot;
+import br.com.vits.orc.vits_agrochain.repository.LotRepository;
+import java.util.List;
+
+@Service
+public class LotService {
+    private final LotRepository lotRepository;
+
+    public LotService(LotRepository lotRepository) {
+        this.lotRepository = lotRepository;
+    }
+
+    public Lot createLot(Lot lot) {
+        return lotRepository.save(lot);
+    }
+
+    public List<Lot> listAll() {
+        return lotRepository.findAll();
+    }
+
+    public Lot getLotById(Long id) {
+        return lotRepository.findById(id).orElse(null);
+    }
+}
